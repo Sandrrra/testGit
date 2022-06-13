30b6ddbe0d3f207dec0cfefabcf54beb9db029c0
ssanc <ssanc@10.81.200.223>
2022-06-13 11:47:34 +0200

    deuxieme commit

--- testGit/src/testGit/Main.java
+++ testGit/src/testGit/Main.java
@@ -2,10 +2,14 @@ package testGit;
 
 public class Main {
 
+	
+	// projet de test des fonctionnalités de Git 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println(" mon projet fonctionne !");
+		System.out.println(" et j'arrive à faire des commit ");
 
 	}
 
