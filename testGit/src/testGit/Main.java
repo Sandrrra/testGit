bba31ed90d6d1a44d0d866926449629ffacf5822
ssanc <ssanc@10.81.200.223>
2022-06-13 09:56:18 +0200

    premier commit
    programme affichant une ligne de texte

---
+++ testGit/src/testGit/Main.java
@@ -0,0 +1,12 @@
+package testGit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" mon projet fonctionne !");
+
+	}
+
+}
